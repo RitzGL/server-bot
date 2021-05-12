15ecf49f923254a9f9fb6608b2970f18621623ec
Ritz GL <maurigarzonlara@hotmail.com>
2021-05-12 17:26:31 +0800

    wrote module for generating random quote from the array, bug fixes remain

--- js/rami-quotes.js
+++ js/rami-quotes.js
@@ -1,22 +1,43 @@
 const fs = require('fs');
 
+function getRandomInt(length) {
+    let int = Math.floor(Math.random() * length);
+    return int;
+}
+
+function generateRandomQuote(quotes){
+    let i = getRandomInt(quotes.length);
+    return quotes[i];
+}
+
 function readTextFile() {
     fs.readFile("./RAMI-QUOTES.txt", "utf8", (err,data) =>{
         if (err){
             console.error(err);
             return;
         }
+
         let quoteArray = data.split("\"");
-        let filteredArray = quoteArray.filter(quote => quote !== '\r\n') // removes \r\n (new line char in Windows)
+        
+        let filteredArray = quoteArray.filter(quote => quote !== '\r\n') // removes weird \r\n fuckery
+        filteredArray = quoteArray.filter(quote => quote !== '\n') // removes \n
+        
         for(let i = 0; i < filteredArray.length; i++){
             filteredArray[i] = filteredArray[i].replace(/'/g, ''); // filters previously added \"
         }
-        console.log(filteredArray);
+        
+        filteredArray.shift(); // removes first item, was empty string
+        filteredArray.pop(); // removes last item, was empty string
+        
+        let quote = generateRandomQuote(filteredArray);
+
+        return quote;
+        // console.log(quote);
+        
+        // the message sending might need to happen here, since it's asynchronous AKA reading from file
+
     })
 }
 
-// readTextFile();
-
-function generateRandomQuote(){
-    
-}
+let quote = readTextFile();
+console.log(quote);
