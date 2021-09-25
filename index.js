8b992d9536dce68d250215ec0eab93be8ccce2dd
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-09-25 11:42:40 +0800

    basic bot working and hooked back into server

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const { Client, Intents } = require('discord.js');
+
+const token = process.env.TOKEN;
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  } else if (commandName === 'server') {
+    await interaction.reply('Server info.');
+  } else if (commandName === 'user') {
+    await interaction.reply('User info.');
+  }
+});
+
+client.login(token);
