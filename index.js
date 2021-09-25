986933452904f31c04e5a2038570b2fdf866017e
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-09-25 16:04:19 +0800

    added logging bot

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-require('dotenv').config();
 const { Client, Intents } = require('discord.js');
+const getCommandResponse = require('./commands/index.js');
+const logger = require('./utils/discordLogger');
 
 const token = process.env.TOKEN;
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-const getCommandResponse = require('./commands');
-
 client.once('ready', () => {
-  console.log('Ready!');
+  console.log('ready');
+  logger.info({
+    message: 'Server Online',
+  });
 });
 
 client.on('interactionCreate', async (interaction) => {
