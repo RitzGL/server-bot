6dd11976578d311a0a59f11e0dbe20410a30ae1c
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-10-03 07:52:17 +0800

    prod db now running in mySQL container

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,38 @@
 const Sequelize = require('sequelize');
 const path = require('path');
 
-const sequelize = new Sequelize('database', 'user', process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'sqlite',
-  logging: true,
-  storage: path.join(__dirname, '..', 'db/database.sqlite'),
-});
+// const localSequelize = new Sequelize(
+//   'database',
+//   'user',
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'sqlite',
+//     logging: true,
+//     storage: path.join(__dirname, '..', 'db/database.sqlite'),
+//   }
+// );
 
-module.exports = sequelize;
+// const prodSequelize = new Sequelize(process.env.DATABASE_URL, {
+//   logging: true,
+// });
+
+function sequelize() {
+  // in production use the linked mySQL container
+  if (process.env.ENV === 'prod') {
+    return new Sequelize(process.env.DATABASE_URL, {
+      logging: true,
+    });
+  }
+  // if running localling user/build an sql db
+  return new Sequelize('database', 'user', process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: true,
+    storage: path.join(__dirname, '..', 'db/database.sqlite'),
+  });
+}
+
+// const sequelize = process.env.ENV === 'prod' ? prodSequelize : localSequelize;
+
+module.exports = sequelize();
